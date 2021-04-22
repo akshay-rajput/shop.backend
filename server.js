50eb0523607135765a61eb486ef561d2c8d7fb3f
Akshay <akshayrajput102@gmail.com>
2021-04-22 23:53:26 +0530

    feat - server - use middlewares and routers

--- server.js
+++ server.js
@@ -1,36 +1,39 @@
 const express = require('express')
 const app = express();
+const requestLog = require('./middleware/requestLog')
+const pageNotFound = require('./middleware/pageNotFound')
+const errorHandler = require('./middleware/errorHandler')
+
 // const bodyParser = require('body-parser')
 // app.use(bodyParser.json());
 
-const products = require('./router/products')
+const productsRouter = require('./router/products')
+const cartRouter = require('./router/cart')
+const wishlistRouter = require('./router/wishlist')
+
 const port = 8000;
 
-app.use('/products', products)
+// log requests
+app.use(requestLog)
+
+// db connection call
+
+
+// route handlers
+app.use('/products', productsRouter)
+app.use('/cart', cartRouter)
+app.use('/wishlist', wishlistRouter)
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
   console.count('\naccessed homepage ')
 });
 
-app.get('/cart', (req, res) => {
-  res.send('cart for..')
-});
-app.get('/wishlist', (req, res) => {
-  res.send('wishlist for ..')
-});
-
-// this should be the last route so it catches
-// any other route which isn't expected
-app.use((req, res) => {
-  res.status(404).json({ success: false, message: "the route you're looking for couldn't be found" })
-})
+// this should be the last route so it catches any other route which isn't expected
+app.use(pageNotFound)
 
 // handle all errors here
-app.use(function (err, req, res, next) {
-  console.error(err.stack)
-  res.status(500).send('Error on server side')
-})
+app.use(errorHandler)
 
 
 app.listen(process.env.PORT || port, () => {
