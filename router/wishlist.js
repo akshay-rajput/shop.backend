f63d8f55410e9c83a3642048ceb60c0e79e5ef14
Akshay <akshayrajput102@gmail.com>
2021-05-04 15:53:58 +0530

    feat - wishlist - use header wishlistid for get post

--- router/wishlist.js
+++ router/wishlist.js
@@ -1,5 +1,7 @@
 let express = require('express');
 let router = express.Router();
+const { Wishlist } = require('../models/wishlist.model');
+const {extend} = require('lodash')
 
 // middleware for wishlist
 router.use(function(req, res, next){
@@ -9,38 +11,93 @@ router.use(function(req, res, next){
 
 // wishlist route
 router.route('/')
-.get(function(req, res){
+.get(async function(req, res){
     try{
-        res.json({
-            success: true, 
-            message: 'Wishlist found'
-        })
+        // if wishlist id given, give wishlist else give list of wishlists
+        if(req.headers.wishlistId){
+            try{
+                let wishlist = await Wishlist.findById(req.headers.wishlistId)
+            
+                res.status(200).json({
+                    success: true, 
+                    message: 'User wishlist found',
+                    wishlist
+                })
+            }
+            catch(err){
+                res.status(404).json({
+                    success: false, 
+                    message: 'wishlist not found',
+                })
+            }
+        }else{
+            try{
+                let wishlistList = await Wishlist.find({})
+            
+                res.status(200).json({
+                    success: true, 
+                    message: 'Showing list of wishlists',
+                    wishlists
+                })
+            }
+            catch(err){
+                res.status(400).json({
+                    success: false,
+                    message: "Cannot get list of wishlists, check your request",
+                    error: err
+                })
+            }
+        }
+        
     }catch(error){
-        console.log('Error getting wishlist ', error)
         res.status(500).json({
             success: false,
             message: 'Server error',
-            errorMessage: error.message
+            errorMessage: error
         })
     }
 
-}).post(function(req, res){
-    // const { name, price } = req.body
-    try{
-        console.log('reqBody: ', req.body)
-        res.status(201).json({ success: true, message: 'wishlist updated' })
-        res.send('wishlist updated')
-    }catch(error){
-        console.log('Error updating wishlist ', error)
-        res.status(500).json({
-            success: false,
-            message: 'Server error updating the wishlist',
-            errorMessage: error.message
-        })
+}).post(async function(req, res){
+    if(req.headers.wishlistId){
+        const wishlistUpdate = req.body
+        try{
+            let wishlist = await Wishlist.findById(req.headers.wishlistId)
+
+            wishlist = extend(wishlist, wishlistUpdate)
+
+            wishlist = await wishlist.save()
+
+            req.status(204).json({
+                success: true,
+                message: "wishlist updated successfully!",
+                wishlist
+            })
+        }catch(error){
+            req.status(404).json({
+                success: false,
+                message: "wishlist not found!",
+                error: error
+            })
+        }
     }
-    // This will happen on DB
-    // const product = { id: idCounter++, name, price }
-    // products.push(product)
+    else{
+        try{
+            let wishlistToSave = req.body
+            wishlistToSave = await wishlistToSave.save()
+
+            res.status(204).json({
+                success: true,
+                message: "wishlist created",
+                wishlist: wishlistToSave
+            })
+        }catch(err){
+            res.status(400).json({
+                success: false,
+                message: "Error while creating wishlist",
+                error: err
+            })
+        }
+    }
 
 })
 
