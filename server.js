9221843d47893262179cc9d12b70054a045c72e9
Akshay <akshayrajput102@gmail.com>
2021-05-04 13:58:57 +0530

    server - connect to db

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express')
 const app = express();
+const { connectDB } = require('./db/db') 
 const requestLog = require('./middleware/requestLog')
 const pageNotFound = require('./middleware/pageNotFound')
 const errorHandler = require('./middleware/errorHandler')
-
-// const bodyParser = require('body-parser')
-// app.use(bodyParser.json());
 
 const productsRouter = require('./router/products')
 const cartRouter = require('./router/cart')
@@ -13,11 +11,14 @@ const wishlistRouter = require('./router/wishlist')
 
 const port = 8000;
 
+// to parse json from req.body
+app.use(express.json())
+
 // log requests
 app.use(requestLog)
 
 // db connection call
-
+connectDB()
 
 // route handlers
 app.use('/products', productsRouter)
@@ -25,8 +26,8 @@ app.use('/cart', cartRouter)
 app.use('/wishlist', wishlistRouter)
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-  console.count('\naccessed homepage ')
+  res.send('API for Shop.Wisp')
+  // console.count('\naccessed homepage ')
 });
 
 // this should be the last route so it catches any other route which isn't expected
