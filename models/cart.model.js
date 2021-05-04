ae7dc8012cf44cf059e701b6abadc0d11fcdddd1
Akshay <akshayrajput102@gmail.com>
2021-05-04 13:57:54 +0530

    refactor - modify cart model to include cart products

--- models/cart.model.js
+++ models/cart.model.js
@@ -1,11 +1,23 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const cartItemSchema = new Schema({
-  _id: { type: Schema.Types.ObjectId, ref: "Product" },
-  qty: Number,
+const cartSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  products: {
+    type: [
+      {
+        productId: { type: Schema.Types.ObjectId, ref: "Product" },
+        qty: {
+          type: Number,
+          min: [1, "Quantity should be greater than or equal to 1"]
+        }
+      }
+    ],
+    required: "Cannot add a cart without products"
+  }
+
 });
 
-const CartItem = mongoose.model("CartItem", cartItemSchema);
+const Cart = mongoose.model("Cart", cartSchema);
 
-module.exports = { CartItem };
+module.exports = { Cart };
