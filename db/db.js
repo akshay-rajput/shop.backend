810e4892d9e9497eaba1219ac24bf033af4cc288
Akshay <akshayrajput102@gmail.com>
2021-05-05 19:14:10 +0530

    fix - db - use node v2 connection string to fix etimeout

--- db/db.js
+++ db/db.js
@@ -4,7 +4,8 @@ require('dotenv').config();
 async function connectDB(){
     console.log('trying to connect to db...')
     try{
-        await mongoose.connect(`mongodb+srv://akshayrajput:${process.env.DB_PASS}@wisp01.6sufq.mongodb.net/Inventory?ssl=true&replicaSet=atlas-zdx3kh-shard-0&authSource=admin&retryWrites=true&w=majority`, {
+        await mongoose.connect(`mongodb://akshayrajput:${process.env.DB_PASS}@wisp01-shard-00-00.6sufq.mongodb.net:27017,wisp01-shard-00-01.6sufq.mongodb.net:27017,wisp01-shard-00-02.6sufq.mongodb.net:27017/Inventory?ssl=true&replicaSet=atlas-zdx3kh-shard-0&authSource=admin&retryWrites=true&w=majority`, {
+        // await mongoose.connect(`mongodb+srv://akshayrajput:${process.env.DB_PASS}@wisp01.6sufq.mongodb.net/Inventory?ssl=true&replicaSet=atlas-zdx3kh-shard-0&authSource=admin&retryWrites=true&w=majority`, {
             useUnifiedTopology: true,
             useNewUrlParser: true
         })
